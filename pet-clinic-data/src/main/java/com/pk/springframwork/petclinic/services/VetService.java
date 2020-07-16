d11f5929188275dd3d766eb48089c680206dbf86
PrashanthKolakani <prashanthkolakani113@gmail.com>
2020-07-17 02:08:12 +0530

    Impl Map based services. closes #6

--- pet-clinic-data/src/main/java/com/pk/springframwork/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/pk/springframwork/petclinic/services/VetService.java
@@ -2,6 +2,6 @@ package com.pk.springframwork.petclinic.services;
 
 import com.pk.springframwork.petclinic.model.Vet;
 
-public interface vetService extends CrudService<Vet, Long> {
+public interface VetService extends CrudService<Vet, Long> {
 
 }
